fe6e432154275e64abf1c750534d7b11453425fa
Laszlo Starost <laszlo.starost@barmer.de>
2024-04-03 12:56:03 +0200

    exercise-01

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -73,14 +73,24 @@ function useUser() {
   return context
 }
 
-// 🐨 add a function here called `updateUser`
-// Then go down to the `handleSubmit` from `UserSettings` and put that logic in
-// this function. It should accept: dispatch, user, and updates
+const updateUser = async (dispatch, user, updates) => {
+  dispatch({type: 'start update', updates: updates})
+  try {
+    const updatedUser = await userClient.updateUser(user, updates)
+    dispatch({type: 'finish update', updatedUser})
+    return updatedUser
+  } catch (error) {
+    dispatch({type: 'fail update', error})
+    throw error;
+  }
+}
 
-// export {UserProvider, useUser}
+const resetUser = dispatch => dispatch({type: 'reset'})
+
+// export {UserProvider, useUser, updateUser, resetUser}
 
 // src/screens/user-profile.js
-// import {UserProvider, useUser} from './context/user-context'
+// import {UserProvider, useUser, updateUser, restUser} from './context/user-context'
 function UserSettings() {
   const [{user, status, error}, userDispatch] = useUser()
 
@@ -97,12 +107,7 @@ function UserSettings() {
 
   function handleSubmit(event) {
     event.preventDefault()
-    // 🐨 move the following logic to the `updateUser` function you create above
-    userDispatch({type: 'start update', updates: formState})
-    userClient.updateUser(user, formState).then(
-      updatedUser => userDispatch({type: 'finish update', updatedUser}),
-      error => userDispatch({type: 'fail update', error}),
-    )
+    updateUser(userDispatch, user, formState).catch(() => {/*do nothing in this case*/})
   }
 
   return (
@@ -149,7 +154,7 @@ function UserSettings() {
           type="button"
           onClick={() => {
             setFormState(user)
-            userDispatch({type: 'reset'})
+            resetUser(userDispatch)
           }}
           disabled={!isChanged || isPending}
         >
